e2e23c74c86bd3b4811159826516c0b5218bba2c
project <smbaharloie@gmail.com>
2021-07-20 12:31:02 +0430

    step_two_add_pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,62 @@
-import { Row, Col, Card } from "antd";
+import { Row, Col, Card, Pagination, Spin } from "antd";
 import React from "react";
 import { useState, useEffect } from "react";
+
 const { Meta } = Card;
 export default function Home() {
   const [latestMovieData, setLatestMovieData] = useState({});
-
+  console.log(latestMovieData);
   const { page, results = [], total_page, total_results } = latestMovieData;
-  useEffect(() => {
+  const [loading, setLoading] = useState(false);
+
+  function fetchMovies(page) {
+    setLoading(true);
     fetch(
-      "https://api.themoviedb.org/3/movie/upcoming?api_key=86ba05b5b5ef9e6cd98405fff0572996&language=en-US"
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=86ba05b5b5ef9e6cd98405fff0572996&language=en-US&page=${page}`
     )
       .then((result) => result.json())
-      .then(setLatestMovieData);
+      .then(setLatestMovieData)
+      .finally(() => setLoading(false));
+  }
+  useEffect(() => {
+    fetchMovies(1);
   }, []);
   return (
-    <Row gutter={[16, 16]}>
-      {results.map((movie) => (
-        <Col xs={24} sm={12} md={8} lg={6} xl={4}>
-          <Card
-            hoverable
-            cover={
-              <img
-                alt={movie.original_title}
-                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              />
-            }
-          >
-            <Meta
-              title={movie.original_title}
-              description="www.instagram.com"
+    <>
+      <Spin spinning={loading}>
+        <Row gutter={[16, 16]}>
+          {results.map((movie) => (
+            <Col key={movie.id} xs={24} sm={12} md={8} lg={6} xl={4}>
+              <Card
+                hoverable
+                cover={
+                  <img
+                    alt={movie.original_title}
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  />
+                }
+              >
+                <Meta
+                  title={movie.original_title}
+                  description="www.instagram.com"
+                />
+              </Card>
+            </Col>
+          ))}
+        </Row>
+        <Row style={{ marginTop: 32 }} justify="center">
+          <Col>
+            <Pagination
+              current={latestMovieData.page}
+              defaultCurrent={1}
+              defaultPageSize={20}
+              total={latestMovieData.total_results}
+              showSizeChanger={false}
+              onChange={fetchMovies}
             />
-          </Card>
-        </Col>
-      ))}
-    </Row>
+          </Col>
+        </Row>
+      </Spin>
+    </>
   );
 }
