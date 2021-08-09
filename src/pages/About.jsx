f32a2d1596fc9887e5459d5afd85c50a102a6f51
project <smbaharloie@gmail.com>
2021-08-09 23:05:15 +0430

    completed-about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,16 @@
+import React from "react";
+import { Divider } from "antd";
 export default function About() {
-  return "salam";
+  return (
+    <>
+     
+      <Divider orientation="right">سلام دوستان</Divider>
+      <p  class="rtl">امیدوارم این سایت برای شما مفید باشه و از امکانات ان لذت ببرید</p>
+      <p   class="rtl">
+        انشاله در اینده نزدیک کاملتر خواهد شد و اطلاعات بیشتری از فیلمها و
+        سریالها در اختیارخواهید داشت
+      </p>
+      <p   class="rtl">با تشکر از اساتید مجرب و متعهد مپصا</p>
+    </>
+  );
 }
